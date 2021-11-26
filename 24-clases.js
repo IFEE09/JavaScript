08554bdeda6968ad038111d2b972cefe7f7be43e
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-25 23:15:29 -0600

    agregando funcion como parametro

--- 24-clases.js
+++ 24-clases.js
@@ -1,3 +1,4 @@
+
 class Persona {
 
     constructor(nombre, apellido, altura) {
@@ -6,8 +7,13 @@ class Persona {
         this.altura = altura;
     }
 
-    saludar() {
+    saludar(responderSaludo) {
         console.log(`Hola mi nombre es ${this.nombre} ${this.apellido}`);
+
+        if (responderSaludo) {
+            responderSaludo(this.nombre, this.apellido, false);
+        }
+
     }
 
     soyAlto() {
@@ -19,17 +25,57 @@ class Persona {
 class Desarrollador extends Persona {
     
     constructor(nombre, apellido, altura) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.altura  = altura;
+        super(nombre, apellido, altura);
     }
     
-    saludar() {
+    saludar(responderSaludo) {
+
         console.log(`Hola mi nombre es ${this.nombre} ${this.apellido} y soy desarrollador`);
+
+        if (responderSaludo) {
+            responderSaludo(this.nombre, this.apellido, true);
+        }
+        
     }
 
+}
+
+function responderSaludo(nombre, apellido, esDev) {
+    console.log(`Buen dia ${nombre} ${apellido}`);
+
+    if(esDev) {
+        console.log(`Excelente, eres desarrollador`);
+    }
+
+}
+
+class Estudiante {
+
+    constructor(nombre, apellido, calificacion){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.calificacion = calificacion;
+    }
+
+    saludar(responderSaludo) {
+
+        //Desestructuracion de la funcion
+        var {nombre, apellido, calificacion} = this;
+
+        console.log(`Hola mi nombre es ${nombre} ${apellido} y tengo calificacion de ${calificacion}`);
+
+        if(responderSaludo){
+            responderSaludo(this.nombre, this.apellido, false)
+        }
+
+
+    }
 
 }
 
 var ivan = new Desarrollador('Ivan', 'Espadas', 1.82);
 var juan = new Persona('Juan', 'Perez', 1.79);
+var pedro = new Estudiante('Pedro', 'Gomez', 9.5);
+
+ivan.saludar(responderSaludo);
+pedro.saludar(responderSaludo);
